d75fbea452e4449241db89e0acafedac13f73006
hankchen7773 <hankchen7773@gmail.com>
2021-01-18 13:45:11 +0800

    wedding_bot_v1.0

---
+++ config.py
@@ -0,0 +1,10 @@
+# imgur key
+client_id = '3cb7dabd0f805d6'
+client_secret = '30a9fb858f64bf4acc1651988e2fbc62e4fedaed'
+album_id = '2aAnwnt'
+access_token = '52945d677b3e985c5e82ba7d1fbd96f52648a1bb'
+refresh_token = '46d87ffac8daa7d8ecd34c85b9db4c64dbc2c582'
+
+# line bot key
+line_channel_access_token = 'ozBtsH1AXPn/0cgAA2HymGPHm5Hx4QYECJ1jU3VuvTOttBCLZe1cwh5DBf8kJBI4H53gH4nIy8fdm1JYHPFRI8+BvNieQ8CcPiMsiv3dz4XKyRFRF2HsCLrH2rs7IIokniUSUL3rgJ2vAL4uNwuglgdB04t89/1O/w1cDnyilFU='
+line_channel_secret = 'd5f3e8acb95897d9414f807fa60a1204'
